6eea3969597af54c6bac36d6622dac7e9dc537eb
Kyryll Radchenko <radch3011@gmail.com>
2023-02-07 20:08:53 +0200

    implement RecyclerView adapter create view of recycler view item

--- app/src/main/java/com/example/redditaddon/utils/RecyclerAdapter.kt
+++ app/src/main/java/com/example/redditaddon/utils/RecyclerAdapter.kt
@@ -9,6 +9,10 @@ import com.example.redditaddon.databinding.ActivityMainBinding
 import com.example.redditaddon.databinding.RecycleviewItemBinding
 import com.example.redditaddon.model.Children
 import com.squareup.picasso.Picasso
+import java.time.Instant
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
+import java.util.*
 
 class RecyclerAdapter (
     private val mainBind: ActivityMainBinding
@@ -29,21 +33,25 @@ class RecyclerAdapter (
 
     override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
         val thumbNail = publications[position].data.thumbnail
+        val thumbNailHeight = publications[position].data.thumbnail_height * 5
+        val thumbNailWidth = publications[position].data.thumbnail_width * 5
+
+        val postTime = countPostTime(position)
 
         val context = holder.itemView.context
 
-
-
         with(holder.binding) {
             author = publications[position].data.subreddit_name_prefixed
-            time = publications[position].data.created_utc.toString()
+            time = postTime
             text = publications[position].data.title
 
-            comments = publications[position].data.num_comments.toString() + "comments"
+            comments = publications[position].data.num_comments.toString() + " comments"
 
             //setting up the photo via Picasso library
             Picasso.with(context)
                 .load(thumbNail)
+                .noPlaceholder()
+                .resize(thumbNailWidth, thumbNailHeight)
                 .into(image)
 
             //if tap on the image open it on fullscreen
@@ -52,11 +60,34 @@ class RecyclerAdapter (
                     .load(thumbNail)
                     .into(mainBind.scaledImg)
 
+                mainBind.mainView.visibility = View.GONE
                 mainBind.scaledLay.visibility = View.VISIBLE
             }
 
         }
 
+    }
+
+    private fun countPostTime(position: Int): String {
+        val curDate = Date.from(Instant.now())
+        val curDataFormatted =  android.text.format.DateFormat.format("MMMM, dd, yyyy HH:mm:ss", curDate).toString()
+
+        val calendar = Calendar.getInstance(Locale.getDefault())
+        calendar.timeInMillis = (publications[position].data.created_utc * 1000L).toLong()
+        val publicationTime: String = android.text.format.DateFormat.format("MMMM, dd, yyyy HH:mm:ss", calendar).toString()
+
+        val dateFormatInput = DateTimeFormatter.ofPattern("MMMM, dd, yyyy HH:mm:ss")
+
+        val curDateParsed = LocalDateTime.parse(curDataFormatted, dateFormatInput)
+        val pubDateParsed = LocalDateTime.parse(publicationTime, dateFormatInput)
+
+        val noOfHours = java.time.Duration.between(pubDateParsed, curDateParsed)
+
+        return if(noOfHours.toHours() > 0) {
+            noOfHours.toHours().toString() + " hours ago"
+        } else {
+            noOfHours.toHours().toString() + " minutes ago"
+        }
     }
 
     override fun getItemCount(): Int = publications.size
